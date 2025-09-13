28adb89dce915162386147894a13f316da5ab992
stormfather0 <ivan.ystskovyna@gmail.com>
2025-09-13 11:29:27 +0300

    update link to connect index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
   import React from 'react';
   import Home from './pages/Home/Home';
   import Footer from './components/Footer/Footer';
+  import '../src/index.css'
 
   const App = () => {
     return (
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 
 import './Home.css'
+
 import React, { useState, useEffect, useRef } from "react";
 
 import Navbar from '../../components/Navbar/Navbar'
